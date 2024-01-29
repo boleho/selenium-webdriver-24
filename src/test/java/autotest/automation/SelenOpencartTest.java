420a105594e0fdc35c0a1cd8434574ea9a68db50
leh <olehb54@gmail.com>
2024-01-29 18:06:12 +0200

    fst run

--- src/test/java/autotest/automation/SelenOpencartTest.java
+++ src/test/java/autotest/automation/SelenOpencartTest.java
@@ -1,18 +1,14 @@
 package autotest.automation;
 
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
+import org.testng.annotations.*;
 
 public class SelenOpencartTest {
     private WebDriver driver;
