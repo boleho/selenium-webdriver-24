e937cb138bdab643c66eee5af69e0ec12b0ddfbb
leh <olehb54@gmail.com>
2024-01-31 13:02:34 +0200

    works

--- src/main/java/autotest/automation/HelloSelenium.java
+++ src/main/java/autotest/automation/HelloSelenium.java
@@ -4,10 +4,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HelloSelenium {
-    public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
 
-        driver.get("https://selenium.dev");
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver;
+        System.setProperty("webdriver.chrome.driver", "./lib/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        //driver.manage().window().setSize(new Dimension(480, 640)); // mobile screen
+        driver.get("https://www.opencart.com/");
+        System.out.println("++=++working le ++==++");
+        Thread.sleep(3000);
 
         driver.quit();
     }
